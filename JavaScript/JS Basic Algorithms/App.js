5e64f464073b05e31a3a568db3e8ebedc2d5e263
Alejandro Santiago <Kaminariz@Alejandros-iMac.local>
2022-09-29 21:07:36 +0200

    Iteration#1

---
+++ JavaScript/JS Basic Algorithms/App.js
@@ -0,0 +1,18 @@
+/*------------------------------------------------**Iteración #1: Variables**------------------------------------*/
+//1.1
+let myFavoriteHero = "Hulk"; 
+console.log (myFavoriteHero);
+
+//1.2
+let x = "50"
+console.log (x);
+
+//1.3
+let h = "5"
+let y = "10"
+console.log (h);
+console.log (y);
+
+//1.4
+let z = [h+y];
+console.log (z)
