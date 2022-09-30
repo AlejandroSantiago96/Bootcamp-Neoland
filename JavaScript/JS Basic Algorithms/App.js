e77ad623a9251afc70bf2decadb79cbaccf8abf0
Alejandro Santiago <Kaminariz@Alejandros-iMac.local>
2022-09-30 19:33:32 +0200

    Iteracion#3

--- JavaScript/JS Basic Algorithms/App.js
+++ JavaScript/JS Basic Algorithms/App.js
@@ -39,3 +39,25 @@ const toy2={name: "Rallo mcKing", price: 29}
 
 /*-----------------------------------------------Iteración #3: Operadores------------------------------------------*/
 //1.1
+    console.log(10*5);
+//1.2
+    console.log(10/2);
+//1.3
+    console.log(15/9);
+//1.4
+    let p=(10);
+    let j=(5);
+    let o=(p + j);
+    console.log(o);
+//1.5
+    let c=(10);
+    let m=(5);
+    let i=(c*m);
+    console.log(i);
+
+
+
+
+
+
+ 
